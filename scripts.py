d3fcae1ce3cdc7ec1a5f2eed0fc04ce58d2eb39c
aaronaferns <aaronferns38@gmail.com>
2026-01-26 14:22:06 -0500

    Added Evaluation with FID

--- scripts.py
+++ scripts.py
@@ -1,53 +1,236 @@
 
 
-import os
 
-import torch
-import torch.nn.functional as F
+
 from itertools import cycle
 from diffusion import diffusionTrainStep, sample
-
 from tqdm import tqdm
+from utils import save_samples
+from cleanfid import fid
+
+import time, torch, os
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 
 
 
 
-def saveCheckPoint(save_folder, exp_no, step, model, optimizer, loss_list):
-    save_dir = save_folder
-    exp_dir = os.path.join(save_dir, str(exp_no))
-    os.makedirs(exp_dir, exist_ok=True)
-    ckpt_path = os.path.join(exp_dir, f"{step}_checkpoint.pt")
-    torch.save({
+best_fid = float("inf")
+
+
+def save_ckpt(model, ema, optimizer, step, loss_list, ckpt_dir, fid_list, fid_value = None):
+    global best_fid
+    os.makedirs(ckpt_dir, exist_ok=True)
+
+    last_path = os.path.join(ckpt_dir, "last.pt")
+    best_path = os.path.join(ckpt_dir, "best_fid.pt")
+
+    ckpt = {
         "step": step,
         "model": model.state_dict(),
-        "optimizer": optimizer.state_dict(),
+        "ema": ema.ema.state_dict(),
+        "optim": optimizer.state_dict(),
+        "fid_list": fid_list,
         "losses": loss_list,
-    }, ckpt_path)
+    }
+    torch.save(ckpt, last_path)
+    if fid_value is not None and fid_value < best_fid:
+        best_fid = fid_value
+        ckpt["best_fid"] = best_fid
+        torch.save(ckpt, best_path)
+        print("Saved new best_fid:", best_fid)
 
 
 
-def train(model, ema, dataLoader, optimizer, loss_fn, num_steps, save_interval, save_folder, exp_no, device):
+def train(
+    main_dir,
+    start_step,
+    real_dir,
+    sample_dir,
+    model, ema, dataLoader,
+    optimizer, loss_fn,
+    exp_no, 
+    sampling_fn,
+    device,
+    soft_N,
+    hard_N,
+    ckpt_dir,
+    save_dir,
+    fid_warmup_steps=5000,
+    fid_interval_N = 50000,
+    fid_interval_n = 10000,
+):
+    
+    #directories
+    ckpt_dir1 = main_dir + f"/{exp_no}/" + ckpt_dir 
+    save_dir1 = main_dir + f"/{exp_no}/" + save_dir 
+    fdir1 = main_dir +"/" + real_dir
+    fdir2 = main_dir + f"/{exp_no}/" + sample_dir 
+    
+    
+    
     model.train()
     data_iter = cycle(dataLoader)
-    pbar = tqdm(range(num_steps), desc="Training")
-    loss_list = []
-    for step in pbar:
-        X0, _ = next(data_iter)
-        loss = diffusionTrainStep(model, ema, optimizer, loss_fn, X0, device)
-        loss_list.append(loss)
-        if (step > 0 and step%save_interval == 0 ) or step == num_steps-1:
-            saveCheckPoint(
-                save_folder=save_folder,
-                exp_no = exp_no,
-                step = step,
-                model = model,
-                optimizer = optimizer,
-                loss_list = loss_list
-                )
-    
-    print("Done Training")
-    return loss_list
-    
+    pbar = tqdm.tqdm(total=None, desc="Training", dynamic_ncols=True)
 
+    loss_list = []
+    fid_list = []  
+    step = start_step
+    
+    try:
+        while True:
+            X0, _ = next(data_iter)
+
+            loss = diffusionTrainStep(model, ema, optimizer, loss_fn, X0, device)
+            loss_list.append(float(loss))
+
+        
+            pbar.update(1)
+            pbar.set_postfix(loss=f"{loss:.4f}", step=step)
+
+            
+
+        
+            do_fid = (step >= fid_warmup_steps) and (step % fid_interval_n == 0)
+
+            if do_fid:
+            
+                t0 = time.time()
+
+                if step % fid_interval_N == 0:
+                    save_samples(
+                                ema.ema,
+                                sampling_fn,
+                                total=hard_N,
+                                batch_size=256,
+                                steps=50,
+                                eta=0.0,
+                                image_size=32,
+                                device=device,
+                                fake_dir = fdir2,
+                                    
+                                )
+                    fid_score = fid.compute_fid(fdir1+"real_hard", fdir2, device=device,
+                                        num_workers=0,     
+                                        batch_size=64,      
+                                        use_dataparallel=False,)
+
+                elif step % fid_interval_n == 0:
+                    save_samples(
+                                ema.ema,
+                                sampling_fn,
+                                total=soft_N,
+                                batch_size=256,
+                                steps=50,
+                                eta=0.0,
+                                image_size=32,
+                                device=device,
+                                fake_dir = fdir2,
+                                    
+                                )
+                    fid_score = fid.compute_fid(fdir1+"real_soft", fdir2, device=device,
+                                                    num_workers=0,     
+                                                    batch_size=64,      
+                                                    use_dataparallel=False,)
+            
+                    
+
+                dt = time.time() - t0
+                fid_list.append((step, float(fid_score)))
+                pbar.write(f"[FID] step={step}  fid={fid_score:.4f}  ({dt:.1f}s)")
+                pbar.set_postfix(loss=f"{loss:.4f}", step=step, fid=f"{fid_score:.3f}")
+                save_ckpt(model, ema, optimizer, step,loss_list, ckpt_dir1, fid_list = fid_list, fid_value=fid_score)
+                save_loss_fid_plots(
+                    ckpt_dir1,
+                    save_dir1,
+                )
+
+            step += 1
+            
+    except KeyboardInterrupt:
+        pbar.write("\n[INFO] Training interrupted. Saving latest checkpoint...")
+        save_ckpt(model, ema, optimizer, step, loss_list, ckpt_dir1, fid_list = fid_list)
+    finally:
+        pbar.close()
+        
+        
+
+
+
+def evaluate_fid(
+    ema,
+    sampling_fn,
+    hard_N,
+    fdir1,
+    fdir2,
+    device,
+    batch_size = 256,
+    steps = 50,
+    eta = 0.0,
+    image_size = 32,
+    ):
+    t0 = time.time()
+    save_samples(
+                ema.ema,
+                sampling_fn = sampling_fn,
+                total=hard_N,
+                batch_size=batch_size,
+                steps=steps,
+                eta=eta,
+                image_size=image_size,
+                device=device,
+                fake_dir = fdir2,
+                    
+                )
+    fid_score = fid.compute_fid(
+                            fdir1+"real_hard", 
+                            fdir2, device=device,
+                            num_workers=0,     
+                            batch_size=64,      
+                            use_dataparallel=False,
+                            )
+    dt = time.time() - t0
+
+    return fid_score, dt
+    
+def save_loss_fid_plots(
+                checkpoint_path,
+                results_save_path,
+                ):
+    
+        
+    os.makedirs(results_save_path, exist_ok=True)
+    loss_plot_path = os.path.join(results_save_path, "loss_plot.png")
+    fid_plot_path = os.path.join(results_save_path, "fid_plot.png")
+
+    #delete any old figures
+    for path in [loss_plot_path, fid_plot_path]:
+        if os.path.exists(path):
+            os.remove(path)
+            
+    ckpt = torch.load(checkpoint_path, map_location="cpu")
+
+    best_fid = ckpt.get("best_fid")
+    loss_list = ckpt.get("losses")
+    fid_list = ckpt.get("fid_list")
+    
+    # ---- Loss plot ----
+    plt.figure()
+    plt.plot(loss_list)
+    plt.xlabel("Training step")
+    plt.ylabel("MSE loss")
+    plt.title(f"DDPM Training Loss (best FID: {best_fid:.4f})")
+    plt.savefig(loss_plot_path, dpi=200, bbox_inches="tight")
+    plt.close()
+    print(f"Saved loss plot to: {loss_plot_path}")
+
+    # ---- FID plot ----
+    plt.figure()
+    plt.plot(fid_list)
+    plt.xlabel("Training step")
+    plt.ylabel("FID")
+    plt.title(f"Fréchet Inception Distance (Best FID: {best_fid:.4f})")
+    plt.savefig(fid_plot_path, dpi=200, bbox_inches="tight")
+    plt.close()
+    print(f"Saved fid plot to: {fid_plot_path}")
--- train_cifar.py
+++ train_cifar.py
@@ -1,47 +1,96 @@
-import pickle
 from models import Unet
 from datasetLoaders import make_cifar10_train_loader
-import torch
 import matplotlib.pyplot as plt
 from scripts import train
 import torch.nn.functional as F
 from ema import EMA
-def main():
-    is_windows = True
+from torchvision.transforms import ToPILImage
+from torchvision.datasets import CIFAR10
+import shutil, os, tqdm, pickle, torch
+from diffusion import ddim_sample
+
+
+
+def load_real_cifar(data_dir, N, soft_N):
+    soft_real_dir = data_dir + "/real_soft"
+    hard_real_dir = data_dir + "/real_hard"
+    if os.path.exists(soft_real_dir): shutil.rmtree(soft_real_dir)
+    if os.path.exists(hard_real_dir): shutil.rmtree(hard_real_dir)
+
     
-    device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    os.makedirs(soft_real_dir, exist_ok=True)
+    os.makedirs(hard_real_dir, exist_ok=True)
+    
+    ds = CIFAR10(root=data_dir, train=True, download=True)
+    for i in tqdm.tqdm(range(N), desc="Saving CIFAR real images", dynamic_ncols=True):
+        img, _ = ds[i]
+    
+        # save all into hard set
+        img.save(f"{hard_real_dir}/{i:05d}.png")
+
+        # save first soft_N into soft set
+        if i < soft_N:
+            img.save(f"{soft_real_dir}/{i:05d}.png")
+    
+    print("saved soft real images to:", soft_real_dir)
+    print("saved hard real images to:", hard_real_dir)
+
+
+def main():
+    
+    N = 10000
+    soft_N = 5000
+    is_windows = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    main_dir = "/kaggle/working"
+    real_dir = "fid"
+    data_not_loaded = True
     
     model = Unet(
-        in_resolution = 32,  # CIFAR-10 images are 32x32
+        in_resolution = 32, 
         input_ch = 3,
         ch = 128,
         output_ch = 3,
-        num_res_blocks = 3,
+        num_res_blocks = 2,
         temb_dim = 256,
-        attn_res = set([16]),  # Attention at lower resolutions for 32x32
+        attn_res = set([16]),  
         dropout = 0.1,
-        ch_mult=[1,2,4,8]
+        ch_mult=[1,2,2,2]
         )
 
     model = model.to(device)
     ema = EMA(model, decay=0.9999, device=device)
+    
     if is_windows: dataLoader = make_cifar10_train_loader(num_workers=0, pin_memory=False)
     else: dataLoader = make_cifar10_train_loader()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4, betas=(0.9, 0.999), weight_decay=0.0 )
-    loss_fn = F.mse_loss
-    NUM_TRAIN_STEPS = 1
-    exp_no = 1
-    save_interval = 100
-    save_folder = "saves"
-    loss_list = train(model, ema,dataLoader, optimizer, loss_fn, NUM_TRAIN_STEPS, save_interval, save_folder, exp_no, device)
     
-    plt.figure()
-    plt.plot(loss_list)
-    plt.xlabel("Training step")
-    plt.ylabel("MSE loss")
-    plt.title("DDPM Training Loss")
-    plt.savefig(f"{exp_no}_loss_curve.png", dpi=200, bbox_inches="tight")
-    plt.show()
+    
+    
+    if data_not_loaded: load_real_cifar(main_dir+"/"+real_dir, N, soft_N)
+    
+    train(  
+            main_dir = main_dir,
+            start_step=0,
+            real_dir=real_dir,
+            sample_dir = "samples",
+            model = model, 
+            ema=ema, 
+            dataLoader=dataLoader,
+            optimizer=torch.optim.AdamW(model.parameters(), lr=2e-4, betas=(0.9, 0.999), weight_decay=0.0 ), 
+            loss_fn=F.mse_loss,
+            exp_no = 1,
+            sampling_fn = ddim_sample,
+            device = device,
+            soft_N=soft_N,
+            ckpt_dir="checkpoints",
+            save_dir = "saves",
+            hard_N=N,
+            fid_warmup_steps=5000,
+            fid_interval_N=50_000,
+            fid_interval_n=10_000
+        )
+    
+
 
 
     
